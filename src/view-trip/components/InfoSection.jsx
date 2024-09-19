eda23cd2d0f4a3d2b7a1fc65db26858951ebfd49
Tejas Zinjade <tejaszinjade2005@gmail.com>
2024-09-19 22:29:02 +0530

    Initial commit

--- src/view-trip/components/InfoSection.jsx
+++ src/view-trip/components/InfoSection.jsx
@@ -1,14 +1,27 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
+import { IoIosSend } from "react-icons/io";
 
 function InfoSection({trip}){
     return( 
         <div>
             <img src="/placeholder.jpg" className='h-[340px] w-full object-cover rounded-xl'/>
-            <div className='mhy-5 flex flex-col gap-2'>
-                <h2 className='font-bold text-2xl'>{trip?.userSelection?.location?.label}</h2> 
+            <div className='flex justify-between items-center'>
+                <div className='mhy-5 flex flex-col gap-2'>
+                    <h2 className='font-bold text-2xl'>{trip?.userSelection?.location?.label}</h2> 
+                
+                    <div className='flex gap-5'>
+                        <h2 className='p-1 px-3 bg-gray-200 rounded-full text-gray-500 text-xs md:text-md'>📅 {trip.userSelection?.noOfDays} Day</h2>
+                        <h2 className='p-1 px-3 bg-gray-200 rounded-full text-gray-500 text-xs md:text-md'>💸 {trip.userSelection?.budget} Budget</h2>
+                        <h2 className='p-1 px-3 bg-gray-200 rounded-full text-gray-500 text-xs md:text-md'>🥂 No. Of Traveller: {trip.userSelection?.traveler}</h2>
+                    </div>
+                
+                </div>
+                <Button>  <IoIosSend/> </Button>
             </div>
+             
         
         </div>
     )
 }
-export default InfoSection
+export default InfoSection  
