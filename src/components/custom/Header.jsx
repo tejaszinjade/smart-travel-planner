ffc4e6aba07503ceeecd7da13028ba05260314fb
Tejas Zinjade <tejaszinjade2005@gmail.com>
2024-10-02 23:23:12 +0530

    Logo Changed

--- src/components/custom/Header.jsx
+++ src/components/custom/Header.jsx
@@ -53,7 +53,7 @@ function Header() {
   
   return(
     <div className='bg-white-bold p-3 shadow-sm flex justify-between items-center px-5 '> 
-        <img src='/logo.svg'/>
+        <img src='/logo.png' className='h-[50px] w-[150px]'/>
         <div>
            {user ?
              <div className='flex items-center gap-3 '>  
