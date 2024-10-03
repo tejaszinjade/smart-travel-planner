94bbbe3edd06cb0fc8d6d18dd66164556d96ccd9
Tejas Zinjade <tejaszinjade2005@gmail.com>
2024-10-03 10:46:23 +0530

    v

--- src/components/custom/Hero.jsx
+++ src/components/custom/Hero.jsx
@@ -8,7 +8,7 @@ function Hero() {
      
         <h1 className ='font-extrabold text-[50px] text-center mt-16'> 
           <span className='text-[#44BEE4]'>Unleash Your Next Adventure: </span> 
-          AI-Crafted Itineraries,  </h1>
+          AI-Crafted Itineraries, Tailored Just for You! </h1>
           <p className='text-xl text-gray-500 text-center'> Your personal trip planner and travel curator, creating custom itineraries tailored to your interest and budget. </p>
           <img src="/limg.png" className='object-contain md:object-scale-down object-cover h-400 w-750 '/>
           <Link to={'/create-trip'}>
